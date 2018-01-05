0f084d4de9d659b01928f2e287433356599a1817
selesdepselesnul <selesdepselesnul@gmail.com>
2018-01-05 16:31:49 +0700

    rename file based of renameWithSeqNum function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,8 @@
 module Main where
+import qualified System.Directory as SysDir
+import qualified System.Environment as SysEnv
+import qualified System.Info as SysInf
+import qualified Data.List as List
 
 renameWithSeqNum :: Int -> String -> [String] -> [(String, String)]
 renameWithSeqNum _ _ [] = []
@@ -8,6 +12,24 @@ renameWithSeqNum i delimeter xs =
          in (src, fst x ++ delimeter ++ src))
   $ zip (map show [i..length xs]) xs
 
+getOsPathDelimeter :: String
+getOsPathDelimeter =
+  if List.isInfixOf "mingw" $ SysInf.os then "\\" else "/"
+
+combineFilePath :: String -> String -> String
+combineFilePath currentDir fileName = currentDir ++ getOsPathDelimeter ++ fileName
+
 main :: IO ()
 main = do
-  putStrLn "init"
+  currentDir <- SysDir.getCurrentDirectory
+  args <- SysEnv.getArgs
+  sequence_ $ fmap (\x -> SysDir.renameFile
+                              (combineFilePath currentDir (fst x))
+                              (combineFilePath currentDir (snd x)))
+                   $ renameWithSeqNum 1 "." args
+  putStrLn "done !"
+  
+
+
+
+
