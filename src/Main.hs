3d3e72444bdfadbf2fb17df110cf34566386a2be
selesdepselesnul <selesdepselesnul@gmail.com>
2018-01-05 13:52:02 +0700

    add delimeter as renameWithSeqNum parameter

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
-renameWithSeqNum :: Int -> [String] -> [(String, String)]
-renameWithSeqNum _ [] = []
-renameWithSeqNum i xs =
+renameWithSeqNum :: Int -> String -> [String] -> [(String, String)]
+renameWithSeqNum _ _ [] = []
+renameWithSeqNum i delimeter xs =
   map
-  (\x -> (snd x, fst x ++ snd x))
+  (\x -> let src = snd x
+         in (src, fst x ++ delimeter ++ src))
   $ zip (map show [i..length xs]) xs
 
 main :: IO ()
